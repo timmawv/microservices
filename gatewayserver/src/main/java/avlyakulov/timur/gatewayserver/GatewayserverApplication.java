d4dc091d9d7add3b0c047c053bea0b3ff970f13d
timuravlyakulov <avlyakulovtimur@gmail.com>
2025-08-21 09:35:49 +0200

    add custom routing for accounts module

--- gatewayserver/src/main/java/avlyakulov/timur/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/avlyakulov/timur/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package avlyakulov.timur.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,14 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	//способ как можно переопределить routing в gateway
+	@Bean
+	public RouteLocator BankRoutConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(path -> path
+						.path("/bank/accounts/**")
+						.filters(filter -> filter.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")) //мы форвардим наш запрос по такому пути
+						.uri("lb://ACCOUNTS"))  //lb - load balancer
+				.build();
+	}
 }
